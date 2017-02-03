15bb3369f4ef83ca7ee7fcccf28a0a869035745e
Donovan Cotter <donovan.cotter@yahoo.com>
2017-02-02 21:14:51 -0500

    initial commit

---
+++ Shop/StoreChatVC.swift
@@ -0,0 +1,26 @@
+//
+//  StoreChatVC.swift
+//  Shop
+//
+//  Created by Donovan Cotter on 1/21/17.
+//  Copyright © 2017 DonovanCotter. All rights reserved.
+//
+
+import UIKit
+
+class StoreChatVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
